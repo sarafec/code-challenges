335472695a13ca85ff995c35853b366b771846b6
sarafec <fecadusa@gmail.com>
2017-09-26 23:32:32 -0700

    add challenge

---
+++ arrays/002-merge-sorted-lists.js
@@ -0,0 +1,34 @@
+/*
+source: Brandverity Mock Interview (9/26/17)
+problem statement:
+Given two sorted arrays, sort into a third array.
+
+input - two sorted arrays
+output - one sorted array
+
+running time - O(n)
+*/
+
+let arr2 = [3,4,5,6,8];
+let arr1 = [9,11,13,14,15,16];
+
+function mergeArrays(arr1, arr2) {
+	let count1 = 0;
+	let count2 = 0;
+	let sortedArr = [];
+
+	while(count1 < arr1.length && count2 < arr2.length){
+		if (arr1[count1] < arr2[count2]) {
+			sortedArr.push(arr1[count1]);
+			count1++;
+		} else {
+			sortedArr.push(arr2[count2]);
+			count2++;
+		}
+	}
+
+	//add functionality for appending the rest of the values to sortedArr
+	return sortedArr;
+}
+
+console.log(mergeArrays(arr1, arr2));
